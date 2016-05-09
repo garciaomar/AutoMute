5eaa683596fd2511b83fa44890d1a561d2251529
Poncho0794 <benjamin.banda.reyna@hotmail.com>
2016-05-08 23:44:51 -0500

    add markers finished, next step save them

--- app/src/main/java/com/example/root/automute/Map.java
+++ app/src/main/java/com/example/root/automute/Map.java
@@ -1,11 +1,17 @@
 package com.example.root.automute;
 
+import android.content.DialogInterface;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -17,11 +23,25 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class Map extends Fragment implements OnMapReadyCallback{
 
     private GoogleMap mMap;
-
+    private FloatingActionButton fab;
+    private  LatLng markerCoo;
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.content_map, container, false);
        SupportMapFragment mapFragment = (SupportMapFragment) this.getChildFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        fab = (FloatingActionButton)v.findViewById(R.id.fab);
+        fab.setEnabled(false);
+        fab.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#757575")));
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            createMarker(markerCoo);
+                fab.setEnabled(false);
+                fab.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#757575")));
+
+            }
+        });
         return v;
     }
 
@@ -41,12 +61,37 @@ public class Map extends Fragment implements OnMapReadyCallback{
         mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
             @Override
             public void onMapClick(LatLng latLng) {
-                mMap.addMarker(new MarkerOptions().position(latLng));
+                fab.setEnabled(true);
+                fab.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#287272")));
+                markerCoo = latLng;
             }
         });
         // Add a marker in Sydney and move the camera
     /*    LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));*/
+    }
+    public void createMarker(LatLng latLng){
+        AlertDialog dialog = createDialog(latLng);
+        dialog.show();
+    }
+    public AlertDialog createDialog(final LatLng latlng){
+        AlertDialog.Builder bld =new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+
+        final View view = getActivity().getLayoutInflater().inflate(R.layout.new_place,null);
+        bld.setView(view).setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                EditText txt = (EditText)view.findViewById(R.id.nwplace);
+                mMap.addMarker(new MarkerOptions().position(latlng).title(txt.getText().toString()));
+            }
+        }).setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        }).setTitle("Agregar Lugar");
+        return bld.create();
     }
 }
